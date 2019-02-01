5664b595387898cca26e00cfc5ff1a73dc1193bb
Kryštof Davídek <36930985+Sedoriko@users.noreply.github.com>
2019-02-01 11:41:37 +0100

    Initial ShareLaTeX Import

---
+++ make.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MAIN="bakalarka"
+
+echo -e "\n\n### Converting Markdown to LaTeX ###\n\n"
+
+for FILE in src/*.md; do
+  pandoc ${FILE} -o "${FILE%.md}.tex"
+  vlna -r -l -v KkSsVvZzOoUuAaIi "${FILE%.md}.tex"
+  sed -ie 's/\s\\parencite/~\\parencite/g' "${FILE%.md}.tex"
+  sed -ie 's/\\section/\\chapter/g' "${FILE%.md}.tex"
+  sed -ie 's/\\subsection/\\section/g' "${FILE%.md}.tex"
+  sed -ie 's/\\subsubsection/\\subsection/g' "${FILE%.md}.tex"
+  sed -ie 's/\\paragraph/\\subsubsection/g' "${FILE%.md}.tex"
+  sed -ie 's/\s\\%/\~\\%/g' "${FILE%.md}.tex"
+
+  sed -ie 's/\\chapter{Úvod}\\label{uxfavod}/'\
+'\\chapter\*{Úvod}\\label{uvod}\n\\addcontentsline{toc}{chapter}{Úvod}/g' "${FILE%.md}.tex"
+
+  sed -ie 's/\\section{Závěr}\\label{zuxe1vux11br}/'\
+'\\chapter\*{Závěr}\\label{zaver}\n\\addcontentsline{toc}{chapter}{Závěr}/g' "${FILE%.md}.tex"
+
+
+  rm "${FILE%.md}.texe"
+done
+
+
+
+
---
+++ src/1-kapitola.md
@@ -0,0 +1,11 @@
+# Tohle je první kapitola
+
+## Tohle je podkapitola
+
+### Tohle taky
+
+#### Co je tohle?
+
+Tohle je text
+
+## A?
